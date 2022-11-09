101fc00f02760c954ccbe300552600d9b30f2563
dl <diego.londono@enterapia.co>
2022-11-09 09:58:51 -0500

    update site

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,35 @@
 module.exports = {
   siteMetadata: {
     title: `d3g0zz`,
-    siteUrl: `https://www.d3g0zz.com`
+    siteUrl: `https://www.d3g0zz.com`,
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "1234567"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "1234567",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto Mono`,
+          `source sans pro\:400`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
+  ],
 };
--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 * {
   box-sizing: border-box;
-  font-family: "Roboto", sans-serif;
+  font-family: "Roboto Mono", sans-serif;
   font-weight: 400;
   font-size: 16px;
   padding: 0;
